3bad64e4b903d04ea4c8d65af5cfd2cc0a11b07a
MaheshB77 <mbansode7696@gmail.com>
2020-09-27 22:46:36 +0530

    Added Google authentication

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from "react";
 import "./App.css";
 import { motion } from "framer-motion";
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import "firebase/analytics";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { useAuthState } from "react-firebase-hooks/auth";
 
+firebase.initializeApp({
+    apiKey: "AIzaSyDmhtZbpO3_4qxp4Z6opVZMt1clPjQYs6w",
+    authDomain: "liviano-live-chat.firebaseapp.com",
+    databaseURL: "https://liviano-live-chat.firebaseio.com",
+    projectId: "liviano-live-chat",
+    storageBucket: "liviano-live-chat.appspot.com",
+    messagingSenderId: "563262955647",
+    appId: "1:563262955647:web:a593b57fa2f035bd5d1f4a",
+    measurementId: "G-K6E916F2CZ"
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const analytics = firebase.analytics();
 function App() {
+    const [user] = useAuthState(auth);
+    let isLoggedIn = user ? <Welcome /> : <PleaseLogin />;
     return (
         <div className="App">
-            <motion.h1
+            {/* <motion.h1
                 animate={{
                     color: "black",
                     scale: 0.9,
@@ -14,9 +36,34 @@ function App() {
                     }
                 }}>
                 Liviano live chat coming soon......
-            </motion.h1>
+            </motion.h1> */}
+            <div>{isLoggedIn}</div>
         </div>
     );
+}
+
+const Welcome = () => {
+    return (
+        <div>
+            <h1>Welcome!!!</h1>
+            <SignOut />
+        </div>
+    );
+};
+function PleaseLogin() {
+    const googleAuth = (event) => {
+        event.preventDefault();
+        const authProvider = new firebase.auth.GoogleAuthProvider();
+        auth.signInWithPopup(authProvider);
+    };
+    return (
+        <div className="PleaseLogin">
+            <button onClick={googleAuth}>Click to sign with google</button>
+        </div>
+    );
+}
+function SignOut() {
+    return <button onClick={() => auth.signOut()}>Sign Out</button>;
 }
 
 export default App;
